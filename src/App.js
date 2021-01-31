4d0ee10c1096ed30aa5d174f6d46838eecccff00
Atharv Wairagade <atharvvw@uw.edu>
2021-01-30 19:23:10 -0800

    can send messages to main screen and other tiny features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
+// import { useState } from 'react';
+// import './App.css';
+// import TextInput from './TextInput'
+// function App() {
+//   const [messages, setMessages] = useState([{text:'hello'}])
+//   return (
+//     <div className="App">
+//       <header className="header">
+//         <div className="logo" />
+//         CHATTER
+//       </header>
+
+//       <div className="message">
+//         {messages[0].text}
+//       </div>
+
+//       <TextInput 
+//         send={(t)=> setMessages([{text:t}])}
+//       />
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState } from 'react';
 import './App.css';
-
+import TextInput from './TextInput';
 function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <div className="logo" />
-        CHATTER
-      </header>
+  const [messages,setMessages] = useState([{text:'hello'}])
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+      CHATTER
+    </header>
+    <div className="message">
+      {messages[0].text}
     </div>
-  );
+    <TextInput 
+      send={(t)=> setMessages([{text:t}])}
+    />
+  </div>
 }
-
 export default App;
