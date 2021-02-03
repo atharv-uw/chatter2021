5a3c1b8bf900d665aa0fbbd70b421d12997aedab
Atharv Wairagade <atharvvw@uw.edu>
2021-02-02 22:03:14 -0800

    updated to class

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
-// import { useState } from 'react';
-// import './App.css';
-// import TextInput from './TextInput'
-// function App() {
-//   const [messages, setMessages] = useState([{text:'hello'}])
-//   return (
-//     <div className="App">
-//       <header className="header">
-//         <div className="logo" />
-//         CHATTER
-//       </header>
-
-//       <div className="message">
-//         {messages[0].text}
-//       </div>
-
-//       <TextInput 
-//         send={(t)=> setMessages([{text:t}])}
-//       />
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { useState } from 'react';
+import {useState} from 'react';
 import './App.css';
 import TextInput from './TextInput';
+import Message from './Message'
+
 function App() {
-  const [messages,setMessages] = useState([{text:'hello'}])
+  const [messages,setMessages] = useState([])
+
   return <div className="App">
+ 
     <header className="header">
       <div className="logo" />
       CHATTER
     </header>
-    <div className="message">
-      {messages[0].text}
-    </div>
+
+    <main className="messages">
+      {messages.map((m,i)=> {
+        return <Message key={i} {...m} />
+      })}
+    </main>
+
     <TextInput 
-      send={(t)=> setMessages([{text:t}])}
+      send={(t)=> setMessages( [{text:t}, ...messages] )}
     />
+
   </div>
 }
+
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,54 +1,34 @@
-// import {useState} from 'react'
-
-// function TextInput(props){
-//     const [text, setText] = useState('')
-    
-//     function sendMessage(){
-//         props.send(text)
-//         setText('')
-//     }
-    
-//     return <footer className="text-input">
-//     <input
-//         placeholder="Write your message"
-//         value={text}
-//         onChange={e=>setText(e.target.value)}
-//     />
-//     <button onClick={sendMessage}>
-//         Send  
-//     </button>
-//     </footer>
-// }
-// export default TextInput
 
 import {useState} from 'react'
+import { AiOutlineSend } from "react-icons/ai";
+
 function TextInput(props) {
   const [text, setText] = useState('')
-  
+
   function sendMessage(){
-    if(text===''){
-        return // skip the function
-    }
+    if(text==='') return // skip the function
     props.send(text)
     setText('')
   }
-
   function keyPressed(e){
-    if(e.key==='Enter'){
-        sendMessage()
+    if(e.key==='Enter') {
+      sendMessage()
     }
   }
 
   return <footer className="text-input">
-    <input 
+    <input className="input"
       placeholder="Write your message"
       value={text}
       onChange={e=> setText(e.target.value)}
       onKeyPress={keyPressed}
     />
-    <button onClick={sendMessage} disabled={!text}>
-      SEND
+    <button onClick={sendMessage} disabled={!text} className="send-button">
+      <AiOutlineSend title="send" 
+        style={{minWidth:'0.7rem',marginLeft:2}} 
+      />
     </button>
   </footer>
 }
+
 export default TextInput
